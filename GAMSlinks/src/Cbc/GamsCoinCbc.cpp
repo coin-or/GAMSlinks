921c3c2328300553e460501f6d4862332c306e29
Stefan Vigerske <svigerske@gams.com>
2007-01-20 20:25:54 +0000

    parameter file reading; own CbcStrategy; cleaning up

--- GAMSlinks/src/Cbc/GamsCoinCbc.cpp
+++ GAMSlinks/src/Cbc/GamsCoinCbc.cpp
@@ -6,11 +6,6 @@
 //
 // Authors:  Michael Bussieck, Stefan Vigerske
 
-#if defined(_MSC_VER)
-// Turn off compiler warning about long names
-#  pragma warning(disable:4786)
-#endif
-
 #include "GAMSlinksConfig.h"
 #include <iostream>
 
@@ -21,13 +16,8 @@
 
 // For Branch and bound
 #include "CbcModel.hpp"
-#include "CbcBranchActual.hpp"
-#include "CbcStrategy.hpp"
-#include "CbcBranchUser.hpp"
-#include "CbcCompareUser.hpp"
-// Heuristics
-#include "CbcHeuristic.hpp"
-#include "CbcHeuristicLocal.hpp"
+#include "CbcBranchActual.hpp" //for CbcSOS
+#include "CbcStrategyGams.hpp"
 
 #include "OsiSolverInterface.hpp"
 #if defined(COIN_HAS_CLP)
@@ -40,36 +30,8 @@
 #error "Clp or Glpk need to be available."
 #endif
 
-// Cuts
-#include "CglPreProcess.hpp"
-#include "CglGomory.hpp"
-#include "CglProbing.hpp"
-#include "CglKnapsackCover.hpp"
-#include "CglOddHole.hpp"
-#include "CglRedSplit.hpp"
-#include "CglClique.hpp"
-#include "CglFlowCover.hpp"
-#include "CglMixedIntegerRounding2.hpp"
+void write_mps(GamsModel& gm, OsiSolverInterface& solver, GamsMessageHandler& myout, char* filename);
 
-void write_mps(GamsModel& gm, OsiSolverInterface& solver, GamsMessageHandler& myout);
-
-/**
-This main program copied in large portions from John Forrest's original
-sample2.cpp.
-
-It sets up some Cgl cut generators and calls branch and cut.
-
-Branching is simple binary branching on integer variables.
-
-Node selection is depth first until first solution is found and then based on
-objective and number of unsatisfied integer variables.  In this example the
-functionality is the same as default but it is a user comparison function.
-
-Variable branching selection is on maximum minimum-of-up-down change after
-strong branching on 5 variables closest to 0.5.
-
-A simple rounding heuristic is used.
-*/
 int main (int argc, const char *argv[]) {
 #if defined(_MSC_VER)
   /* Prevents hanging "Application Error, Click OK" Windows in case something bad happens */
@@ -81,6 +43,7 @@ int main (int argc, const char *argv[]) {
 		exit(EXIT_FAILURE);
 	}	
 	int i,j;
+	char buffer[255];
 
 	OsiXXXSolverInterface solver;
 
@@ -96,28 +59,25 @@ int main (int argc, const char *argv[]) {
 	solver.passInMessageHandler(&slvout);
 	solver.setHintParam(OsiDoReducePrint,true,OsiHintTry);
 	
-	myout << "\nGAMS/CoinCbc Lp/Mip Solver\nwritten by J.Forrest\n " << CoinMessageEol;
+	myout << "\nGAMS/CoinCbc 1.1.0 LP/MIP Solver\nwritten by J.Forrest\n " << CoinMessageEol;
 	
-//	char options[][32]={"cut generation", "rounding heuristic", "local search", "strong branching", "integer presolve"};
-//	char cutnames[][32]={"Probing", "Gomory", "Knapsack", "OddHole", "Clique", "FlowCover", "MIR", "RedSplit"};
-//
-//	myout << "\nOptions:" << CoinMessageEol;
-//	for (i=0; i<4; i++) { // We don't want to advertise integer presolve
-//	  int onoff=gm.getGamsSwitch(1,i)? 0:1;
-//    myout << "  " << options[i] << "\t";
-//	  if (0==i && onoff) {
-//	    for (j=0; j<8; j++) 
-//	    	switch (j) {
-//	    		case 3: case 7: 
-//				    if (1==gm.getGamsSwitch(2,j)) myout << cutnames[j];
-//				    break;
-//				  default:
-//	    			if (0==gm.getGamsSwitch(2,j)) myout << cutnames[j];
-//	  		}
-//	    myout << CoinMessageEol;
-//	  } else
-//	    myout << (onoff? "on":"off") << CoinMessageEol;      
-//	}
+#ifdef GAMS_BUILD	
+	if (!gm.ReadOptionsDefinitions("coincbc"))
+#else
+	if (!gm.ReadOptionsDefinitions("cbc"))
+#endif
+		myout << "Error intializing option file handling or reading option file definitions!" << CoinMessageEol
+			<< "Processing of options is likely to fail!" << CoinMessageEol;  
+	gm.ReadOptionsFile();
+
+	/* Overwrite GAMS Options */
+	if (!gm.optDefined("reslim")) gm.optSetDouble("reslim", gm.getResLim());
+	if (!gm.optDefined("iterlim")) gm.optSetInteger("iterlim", gm.getIterLim());
+	if (!gm.optDefined("nodelim")) gm.optSetInteger("nodelim", gm.optDefined("nodlim") ? gm.optGetInteger("nodlim") : gm.getNodeLim());
+	if (!gm.optDefined("nodlim")) gm.optSetInteger("nodlim", gm.getNodeLim());
+	if (!gm.optDefined("optca")) gm.optSetDouble("optca", gm.getOptCA());
+	if (!gm.optDefined("cutoff") && gm.getCutOff()!=gm.ObjSense()*solver.getInfinity()) gm.optSetDouble("cutoff", gm.getCutOff());
+	
 	gm.TimerStart();
 
 	// CLP needs rowrng for the loadProblem call
@@ -142,11 +102,11 @@ int main (int argc, const char *argv[]) {
 	  if (discVar[j]) solver.setInteger(j);
 
 	// Write MPS file
-	if (gm.getGamsInteger(0))
-		write_mps(gm, solver, myout);
+	if (gm.optDefined("writemps"))
+		write_mps(gm, solver, myout, gm.optGetString("writemps", buffer));
 
 	// Some tolerances and limits
-	solver.setIntParam(OsiMaxNumIteration, gm.getIterLim());
+	solver.setIntParam(OsiMaxNumIteration, gm.optGetInteger("iterlim"));
 #if (OsiXXXSolverInterface == OsiClpSolverInterface)
 	solver.getModelPtr()->setDualBound(1.0e10);
 #endif
@@ -166,12 +126,16 @@ int main (int argc, const char *argv[]) {
 			for (j=0; j<gm.nCols(); ++j)
 				if (gm.SOSIndicator()[j]==i) which[n++]=j;
 			objects[i-1]=new CbcSOS(&model, n, which, NULL, i-1, 1);
+			// branch on long sets first
+			objects[i-1]->setPriority(gm.nCols()-n);
 		}
 		for (i=1; i<=gm.nSOS2(); ++i) {
 			int n=0;
 			for (j=0; j<gm.nCols(); ++j)
 				if (gm.SOSIndicator()[j]==-i) which[n++]=j;
 			objects[gm.nSOS1()+i-1]=new CbcSOS(&model, n, which, NULL, gm.nSOS1()+i-1, 2);
+			// branch on long sets first
+			objects[gm.nSOS1()+i-1]->setPriority(gm.nCols()-n);
 		}
 		delete[] which;
 		
@@ -181,81 +145,17 @@ int main (int argc, const char *argv[]) {
 		delete[] objects;
   }
 
-	model.setIntParam(CbcModel::CbcMaxNumNode, gm.getNodeLim());
-	model.setDblParam(CbcModel::CbcMaximumSeconds, gm.getResLim());
-	model.setDblParam(CbcModel::CbcAllowableGap, gm.getOptCA());
-	model.setCutoff(gm.ObjSense()*gm.getCutOff());
-
-	// Set up some cut generators and defaults
-	// Probing first as gets tight bounds on continuous
-	CglProbing generator1;
-	generator1.setUsingObjective(true);
-	generator1.setMaxPass(1);
-	generator1.setMaxPassRoot(5);
-//	generator1.setMaxProbe(10);
-	generator1.setMaxProbe(1000);
-	generator1.setMaxLook(50);
-	generator1.setMaxLookRoot(500);
-	generator1.setMaxElements(200);
-	generator1.setRowCuts(3);
-
-	CglGomory generator2;
-	generator2.setLimit(300);   // try larger limit
-
-	CglKnapsackCover generator3;
-
-	CglOddHole generator4;
-	generator4.setMinimumViolation(0.005);
-	generator4.setMinimumViolationPer(0.00002);
-	generator4.setMaximumEntries(200);   // try larger limit
-
-	CglClique generator5;
-	generator5.setStarCliqueReport(false);
-	generator5.setRowCliqueReport(false);
-
-	CglRedSplit generator6;
-	// try larger limit
-	generator6.setLimit(200);
-
-	CglMixedIntegerRounding2 mixedGen;
-	CglFlowCover flowGen;
-	
-	if (0==gm.getGamsSwitch(1,0)) {  // m.integer1 = 1 turns cut generation off
-	  // Add in generators
-	  if (0==gm.getGamsSwitch(2,0)) model.addCutGenerator(&generator1,-1,"Probing");
-	  if (0==gm.getGamsSwitch(2,1)) model.addCutGenerator(&generator2,-1,"Gomory");
-	  if (0==gm.getGamsSwitch(2,2)) model.addCutGenerator(&generator3,-1,"Knapsack");
-	  if (1==gm.getGamsSwitch(2,3)) model.addCutGenerator(&generator4,-1,"OddHole");
-	  if (0==gm.getGamsSwitch(2,4)) model.addCutGenerator(&generator5,-1,"Clique");
-	  if (0==gm.getGamsSwitch(2,5)) model.addCutGenerator(&flowGen,-1,"FlowCover");
-	  if (0==gm.getGamsSwitch(2,6)) model.addCutGenerator(&mixedGen,-1,"MixedIntegerRounding2");
-	  if (1==gm.getGamsSwitch(2,7)) model.addCutGenerator(&generator6,-1,"RedSplit");
-	}
-
-	// Rounding heuristic
-	CbcRounding heuristic1(model);
-	// m.integer2 = 1 turns rounding heuristic off
-	if (0==gm.getGamsSwitch(1,1)) 
-	  model.addHeuristic(&heuristic1); 
-
-	// And local search when new solution found
-	CbcHeuristicLocal heuristic2(model);
-	// m.integer3 = 1 turns local search off
-	if (0==gm.getGamsSwitch(1,2)) 
-	  model.addHeuristic(&heuristic2); 
-
-	// Redundant definition of default branching (as Default == User)
-	CbcBranchUserDecision branch;
-	model.setBranchingMethod(&branch);
-
-	// Definition of node choice
-	CbcCompareUser compare;
-	model.setNodeComparison(compare);
+	model.setIntParam(CbcModel::CbcMaxNumNode, gm.optGetInteger("nodelim"));
+	model.setDblParam(CbcModel::CbcMaximumSeconds, gm.optGetDouble("reslim"));
+	model.setDblParam(CbcModel::CbcAllowableGap, gm.optGetDouble("optca"));
+	if (gm.optDefined("cutoff")) model.setCutoff(gm.ObjSense()*gm.optGetDouble("cutoff")); // Cbc assumes a minimizatio problem here
 
 	// Do initial solve to continuous
-	model.solver()->messageHandler()->setLogLevel(2);
 	if (gm.nDCols() || gm.nSOS1() || gm.nSOS2())
-	  myout << "\nSolving the root node..." << CoinMessageEol;
+	  myout << CoinMessageEol << "Solving the root node..." << CoinMessageEol;
+	buffer[0]=0; gm.optGetString("startalg", buffer);
+	model.solver()->setHintParam(OsiDoDualInInitial, strcmp(buffer, "primal")==0 ? false : true);
+	model.solver()->messageHandler()->setLogLevel(4);
 	model.initialSolve();
 
 	if (0==gm.nDCols() && 0==gm.nSOS1() && 0==gm.nSOS2()) {  // If this was an LP we are done
@@ -268,48 +168,12 @@ int main (int argc, const char *argv[]) {
 	  return 0;
 	}
 	
-	// minimum drop to continue cuts
-	model.setMinimumDrop(min(1.0, fabs(model.getObjValue())*1.0e-3+1.0e-4));
-
-	if (model.getNumCols()<500)
-	  model.setMaximumCutPassesAtRoot(-100); // always do 100 if possible
-	else if (model.getNumCols()<5000)
-	  model.setMaximumCutPassesAtRoot(100); // use minimum drop
-	else
-	  model.setMaximumCutPassesAtRoot(20);
-	model.setMaximumCutPasses(10);
-
-	// Do more strong branching if small
-	if (model.getNumCols()<5000)
-	  model.setNumberStrong(10);
-	model.setNumberBeforeTrust(5);
-
-	// m.integer4 = 1 turns strong branching off
-	if (1==gm.getGamsSwitch(1,3)) model.setNumberStrong(0);
-
 	model.solver()->setIntParam(OsiMaxNumIterationHotStart,100);
 
-	if (gm.getSysOut()) {
-	  model.messageHandler()->setLogLevel(4);
-	  model.solver()->messageHandler()->setLogLevel(4);
-	}	else {
-	  model.messageHandler()->setLogLevel(2);
-	  model.solver()->messageHandler()->setLogLevel(0);
-	}
-	model.setPrintFrequency(10);
-
-	// Default strategy will leave cut generators as they exist already
-	// so cutsOnlyAtRoot (1) ignored
-	// numberStrong (2) is 5 (default)
-	// numberBeforeTrust (3) is 5 (default is 0)
-	// printLevel (4) defaults (0)
-	CbcStrategyDefault strategy(true,5,5);
-	// Set up pre-processing to find sos if wanted
-	if (0==gm.getGamsSwitch(1,4))
-	  strategy.setupPreProcessing(2);
+	CbcStrategyGams strategy(gm);
 	model.setStrategy(strategy);
 	
-	myout << "Starting branch-and-bound..." << CoinMessageEol;
+	myout << CoinMessageEol << "Starting branch-and-bound..." << CoinMessageEol;
 	model.branchAndBound();
 
 	myout << "Cbc Status: " << model.status() << CoinMessageEol;
@@ -317,9 +181,9 @@ int main (int argc, const char *argv[]) {
 	GamsFinalizeOsi(&gm, &myout, model.solver(), 0);
 
 	return 0;
-}    
+}
 
-void write_mps(GamsModel& gm, OsiSolverInterface& solver, GamsMessageHandler& myout) {
+void write_mps(GamsModel& gm, OsiSolverInterface& solver, GamsMessageHandler& myout, char* filename) {
 	char namebuf[10];
 	const char **colnames=new const char *[gm.nCols()];
 	const char **rownames=new const char *[gm.nRows()];
@@ -335,8 +199,8 @@ void write_mps(GamsModel& gm, OsiSolverInterface& solver, GamsMessageHandler& my
     rownames[j] = strdup(namebuf); 
   }
 
-  myout << "\nWriting MPS file coinprob.mps... " << CoinMessageEol;
-  solver.writeMpsNative("coinprob.mps",rownames,colnames,0,2,gm.ObjSense());
+  myout << "\nWriting MPS file " << filename << "... " << CoinMessageEol;
+  solver.writeMpsNative(filename,rownames,colnames,0,2,gm.ObjSense());
 
   // We don't need these guys anymore
   for (j=gm.nRows()-1; j>=0; j--)
---
+++ GAMSlinks/src/Cbc/optcbc.gms
@@ -0,0 +1,85 @@
+$setglobal PDFLINK none
+$eolcom //
+set g Cbc Option Groups /
+        general        General Options
+      /
+    e / '-1', 0*100, '+n', primal, dual, barrier /
+    f / def Default, lo Lower Bound, up Upper Bound, ref Reference /
+    t / I Integer, R Real, S String, B Binary /
+    o Options /
+      cuts                   switch for cut generation 
+      roundingheuristic      rounding heuristic
+      localsearch            local search
+      strongbranching        strong branching
+      integerpresolve        integer presolve
+      findsos                find sos
+      probing                probing cuts
+      gomorycuts             Gomory cuts
+      knapsackcuts           knapsack cuts
+      oddholecuts            odd hole cuts
+      cliquecuts             clique cuts
+      flowcovercuts          flow cover cuts
+      mircuts        		     mixed integer rounding cuts
+      redsplitcuts           reduce and split cuts
+      cutsonlyatroot         whether cuts are only generated at the root node
+      startalg               LP solver for root node
+      writemps               create MPS file for problem
+* GAMS options
+      reslim                 resource limit
+      iterlim                iteration limit
+      nodelim                node limit
+      nodlim                 node limit
+      optca                  absolute stopping tolerance
+      cutoff                 cutoff for tree search
+* immediates
+      nobounds               ignores bounds on options
+      readfile               read secondary option file
+ /
+
+$onembedded
+    optdata(g,o,t,f) /
+general.(
+  cuts                .i.(def 0, lo -1, up 1)
+  roundingheuristic   .b.(def 1)
+  localsearch         .b.(def 1)
+  strongbranching     .b.(def 1)
+  integerpresolve     .b.(def 1)
+  findsos             .b.(def 1)
+  probing             .b.(def 1)
+  gomorycuts          .b.(def 1)
+  knapsackcuts        .b.(def 1)
+  oddholecuts         .b.(def 0)
+  cliquecuts          .b.(def 1)
+  flowcovercuts       .b.(def 1)
+  mircuts             .b.(def 1)
+  redsplitcuts        .b.(def 0)
+  cutsonlyatroot      .b.(def 1)
+  startalg            .s.(def primal)
+  writemps            .s.(def '')
+* GAMS options
+  reslim          .r.(def 1000)
+  iterlim         .i.(def 10000)
+  nodelim         .i.(def maxint)
+  nodlim          .i.(def maxint)
+  optca           .r.(def 0)
+  cutoff          .r.(def 0)
+* immediates
+  nobounds        .b.(def 0)
+  readfile        .s.(def '')
+) /
+  oe(o,e) /
+  cuts.(         '-1'     no cuts will be generated
+                   0      automatic
+                   1      cuts from all available cut classes will be generated )
+  startalg.(      primal  primal simplex algorithm
+                  dual    dual simplex algorithm )
+ /
+ im  immediates recognized  / EolFlag , ReadFile, Message, NoBounds /
+ immediate(o,im)   / NoBounds.NoBounds, ReadFile.ReadFile /
+ hidden(o)         / NoBounds, ReadFile /
+ odefault(o)       / reslim     'GAMS reslim'
+                     iterlim    'GAMS iterlim' 
+                     nodelim    'GAMS nodlim' 
+                     nodlim     'GAMS nodlim' 
+                     optca      'GAMS optca' 
+                     cutoff     'GAMS cutoff' /
