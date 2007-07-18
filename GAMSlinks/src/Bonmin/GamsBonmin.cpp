f94f5c4f34eef36e39bf8f11da93b43a1b2f8edc
Stefan Vigerske <svigerske@gams.com>
2007-07-18 00:13:56 +0000

    catch TNLPSolver::UnsolvedError

--- GAMSlinks/src/Bonmin/GamsBonmin.cpp
+++ GAMSlinks/src/Bonmin/GamsBonmin.cpp
@@ -54,13 +54,13 @@ int main (int argc, char* argv[]) {
   prob = smagInit (argv[1]);
   if (!prob) {
   	fprintf(stderr, "Error reading control file %s\nexiting ...\n", argv[1]);
-		return EXIT_FAILURE;  	
+		return EXIT_FAILURE;
   }
-  
+
   char buffer[512];
   if (smagStdOutputStart(prob, SMAG_STATUS_OVERWRITE_IFDUMMY, buffer, sizeof(buffer)))
   	fprintf(stderr, "Warning: Error opening GAMS output files .. continuing anyhow\t%s\n", buffer);
-  
+
   smagReadModelStats (prob);
   smagSetObjFlavor (prob, OBJ_FUNCTION);
   smagSetSqueezeFreeRows (prob, 1);	/* don't show me =n= rows */
@@ -100,12 +100,12 @@ void solve_minlp(smagHandle_t prob) {
 	SmartPtr<Journalist> journalist= new Journalist();
   SmartPtr<RegisteredOptions> roptions = new RegisteredOptions();
  	SmartPtr<Journal> smag_jrnl=new SmagJournal(prob, "console", J_ITERSUMMARY, J_STRONGWARNING);
-	smag_jrnl->SetPrintLevel(J_DBG, J_NONE);  	
+	smag_jrnl->SetPrintLevel(J_DBG, J_NONE);
 	if (!journalist->AddJournal(smag_jrnl))
 		smagStdOutputPrint(prob, SMAG_ALLMASK, "Failed to register SmagJournal for IPOPT output.\n");
 	options->SetJournalist(journalist);
 	options->SetRegisteredOptions(roptions);
-	
+
 	bonmin_setup.setOptionsAndJournalist(roptions, options, journalist);
   bonmin_setup.registerOptions();
 
@@ -122,7 +122,7 @@ void solve_minlp(smagHandle_t prob) {
 	else
 		bonmin_setup.options()->SetIntegerValue("bonmin.node_limit", prob->gms.itnlim);
 	bonmin_setup.options()->SetNumericValue("bonmin.time_limit", prob->gms.reslim);
-	if (prob->logOption) { // if user wants output, increase default log level a bit 
+	if (prob->logOption) { // if user wants output, increase default log level a bit
 		bonmin_setup.options()->SetIntegerValue("bonmin.bb_log_level", 1);
 		bonmin_setup.options()->SetIntegerValue("bonmin.lp_log_level", 1);
 	}
@@ -139,28 +139,22 @@ void solve_minlp(smagHandle_t prob) {
 	}
 
 	bonmin_setup.options()->GetNumericValue("diverging_iterates_tol", mysmagminlp->div_iter_tol, "");
-//	// or should we also check the tolerance for acceptable points?  
-//	bonmin_setup.options()->GetNumericValue("tol", mysmagminlp->scaled_conviol_tol, ""); 
-//	bonmin_setup.options()->GetNumericValue("constr_viol_tol", mysmagminlp->unscaled_conviol_tol, ""); 
+//	// or should we also check the tolerance for acceptable points?
+//	bonmin_setup.options()->GetNumericValue("tol", mysmagminlp->scaled_conviol_tol, "");
+//	bonmin_setup.options()->GetNumericValue("constr_viol_tol", mysmagminlp->unscaled_conviol_tol, "");
 
 	// the easiest would be to call bonmin_setup.initializeBonmin(smagminlp), but then we cannot set the message handler
 	// so we do the following
 //	bonmin_setup.use(smagminlp); // this initialize the OsiTMINLPInterface
 	SmagMessageHandler smagmessagehandler(prob);
+	try {
 	{
 		OsiTMINLPInterface first_osi_tminlp;
 		first_osi_tminlp.initialize(roptions, options, journalist, smagminlp);
 		first_osi_tminlp.passInMessageHandler(&smagmessagehandler);
 		bonmin_setup.initialize(first_osi_tminlp); // this will clone first_osi_tminlp
-	} 
-//	bonmin_setup.gatherParametersValues(bonmin_setup.options());
-//	bonmin_setup.options()->SetStringValue("bonmin.algorithm", "B-BB");
-//	if (bonmin_setup.getAlgorithm() == Bonmin::B_BB)
-//		bonmin_setup.initializeBBB();
-//	else
-//		bonmin_setup.initializeBHyb(true);
+	}
 
-  try {
 		Bab2 bb;
 
 		mysmagminlp->clock_start=smagGetCPUTime(prob);
@@ -173,17 +167,17 @@ void solve_minlp(smagHandle_t prob) {
 				if (prob->colType[i]!=SMAG_VAR_CONT)
 					osi_tminlp.setColBounds(i, bb.bestSolution()[i], bb.bestSolution()[i]);
 				else if ((!has_free_var) && osi_tminlp.getColUpper()[i]-osi_tminlp.getColLower()[i]>1e-5)
-					has_free_var=true; 
+					has_free_var=true;
 			osi_tminlp.setColSolution(bb.bestSolution());
 
 			if (!has_free_var) {
 				smagStdOutputPrint(prob, SMAG_LOGMASK, "\nAll variables are discrete. Dual variables for fixed problem will be not available.\n");
 				osi_tminlp.initialSolve(); // this will only evaluate the constraints, so we get correct row levels
 				write_solution_nodual(prob, osi_tminlp, mysmagminlp->model_status, mysmagminlp->solver_status, smagGetCPUTime(prob)-mysmagminlp->clock_start, mysmagminlp->domviolations, bb.numNodes());
-			} else { 
+			} else {
 				smagStdOutputPrint(prob, SMAG_LOGMASK, "\nResolve with fixed discrete variables to get dual values.\n");
 				osi_tminlp.initialSolve();
-				if (osi_tminlp.isProvenOptimal()) 
+				if (osi_tminlp.isProvenOptimal())
 					write_solution(prob, osi_tminlp, mysmagminlp->model_status, mysmagminlp->solver_status, smagGetCPUTime(prob)-mysmagminlp->clock_start, mysmagminlp->domviolations, bb.numNodes());
 				else
 					smagStdOutputPrint(prob, SMAG_LOGMASK, "Problems solving fixed problem.\n");
@@ -196,6 +190,11 @@ void solve_minlp(smagHandle_t prob) {
   	snprintf(buf, 1024, "%s::%s\n%s\n", error.className().c_str(), error.methodName().c_str(), error.message().c_str());
 		smagStdOutputPrint(prob, SMAG_ALLMASK, buf);
 	  smagReportSolBrief(prob, 13, 13);
+  } catch (TNLPSolver::UnsolvedError *E) { 	    //There has been a failure to solve a problem with Ipopt.
+	  char buf[1024];
+	  	snprintf(buf, 1024, "Error: %s exited with error %s\n", E->solverName().c_str(), E->errorName().c_str());
+			smagStdOutputPrint(prob, SMAG_ALLMASK, buf);
+		  smagReportSolBrief(prob, 13, 13);
   }
 } // solve_minlp()
 
@@ -205,7 +204,7 @@ void write_solution(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, int model
 	int n=smagColCount(prob);
 	int m=smagRowCount(prob);
 	int isMin=smagMinim(prob);
-	
+
 	const double* lambda=osi_tminlp.getRowPrice();
 	const double* z_L=lambda+m;
 	const double* z_U=z_L+n;
@@ -220,7 +219,7 @@ void write_solution(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, int model
 		if (z_L[i]>-prob->inf) colMarg[i]+=isMin*z_L[i];
 		if (z_U[i]<prob->inf) colMarg[i]-=isMin*z_U[i];
 	}
-	
+
 	unsigned char* rowBasStat=new unsigned char[m];
 	unsigned char* rowIndic=new unsigned char[m];
 	double* negLambda=new double[m];
@@ -233,7 +232,7 @@ void write_solution(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, int model
 		nodeuse, resuse, osi_tminlp.getObjValue()*isMin, domviol,
 		osi_tminlp.getRowActivity(), negLambda, rowBasStat, rowIndic,
 		osi_tminlp.getColSolution(), colMarg, colBasStat, colIndic);
-		
+
 	delete[] colBasStat;
 	delete[] colIndic;
 	delete[] colMarg;
@@ -245,14 +244,14 @@ void write_solution(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, int model
 void write_solution_nodual(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, int model_status, int solver_status, double resuse, int domviol, int nodeuse) {
 	int n=smagColCount(prob);
 	int m=smagRowCount(prob);
-	
+
 	unsigned char* colBasStat=new unsigned char[n];
 	unsigned char* colIndic=new unsigned char[n];
 	for (Index i=0; i<n; ++i) {
 		colBasStat[i]=SMAG_BASSTAT_SUPERBASIC;
 		colIndic[i]=SMAG_RCINDIC_OK;
 	}
-	
+
 	unsigned char* rowBasStat=new unsigned char[m];
 	unsigned char* rowIndic=new unsigned char[m];
   for (Index i = 0;  i < m;  i++) {
@@ -263,7 +262,7 @@ void write_solution_nodual(smagHandle_t prob, OsiTMINLPInterface& osi_tminlp, in
 		nodeuse, resuse, osi_tminlp.getObjValue()*smagMinim(prob), domviol,
 		osi_tminlp.getRowActivity(), prob->rowPi, rowBasStat, rowIndic,
 		osi_tminlp.getColSolution(), prob->colRC, colBasStat, colIndic);
-		
+
 	delete[] colBasStat;
 	delete[] colIndic;
 	delete[] rowBasStat;
@@ -280,9 +279,9 @@ void solve_nlp(smagHandle_t prob) {
   SmartPtr<TNLP> smagnlp = mysmagnlp;
   // Create a new instance of IpoptApplication (use a SmartPtr, not raw)
   SmartPtr<IpoptApplication> app = new IpoptApplication(false);
-  
+
  	SmartPtr<Journal> smag_jrnl=new SmagJournal(prob, "console", J_ITERSUMMARY);
-	smag_jrnl->SetPrintLevel(J_DBG, J_NONE);  	
+	smag_jrnl->SetPrintLevel(J_DBG, J_NONE);
 	if (!app->Jnlst()->AddJournal(smag_jrnl))
 		smagStdOutputPrint(prob, SMAG_ALLMASK, "Failed to register SmagJournal for IPOPT output.\n");
 
@@ -300,12 +299,12 @@ void solve_nlp(smagHandle_t prob) {
 		app->Initialize(prob->gms.optFileName);
 	else
 		app->Initialize("");
-		
+
 	app->Options()->GetNumericValue("diverging_iterates_tol", mysmagnlp->div_iter_tol, "");
-	// or should we also check the tolerance for acceptable points?  
-	app->Options()->GetNumericValue("tol", mysmagnlp->scaled_conviol_tol, ""); 
-	app->Options()->GetNumericValue("constr_viol_tol", mysmagnlp->unscaled_conviol_tol, ""); 
-	
+	// or should we also check the tolerance for acceptable points?
+	app->Options()->GetNumericValue("tol", mysmagnlp->scaled_conviol_tol, "");
+	app->Options()->GetNumericValue("constr_viol_tol", mysmagnlp->unscaled_conviol_tol, "");
+
   // Ask Ipopt to solve the problem
   ApplicationReturnStatus status = app->OptimizeTNLP(smagnlp);
 
@@ -359,7 +358,7 @@ void solve_nlp(smagHandle_t prob) {
 //     Search_Direction_Becomes_Too_Small=3,
 //     Diverging_Iterates=4,
 //     User_Requested_Stop=5,
-// 
+//
 //     Maximum_Iterations_Exceeded=-1,
 //     Restoration_Failed=-2,
 //     Error_In_Step_Computation=-3,
@@ -367,7 +366,7 @@ void solve_nlp(smagHandle_t prob) {
 //     Invalid_Problem_Definition=-11,
 //     Invalid_Option=-12,
 //     Invalid_Number_Detected=-13,
-// 
+//
 //     Unrecoverable_Exception=-100,
 //     NonIpopt_Exception_Thrown=-101,
 //     Insufficient_Memory=-102,
