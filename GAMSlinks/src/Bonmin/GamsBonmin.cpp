cd8efaa82dbbc0df76ac670a0b86f456f738a202
Stefan Vigerske <svigerske@gams.com>
2008-09-13 13:57:25 +0000

    take out note

--- GAMSlinks/src/Bonmin/GamsBonmin.cpp
+++ GAMSlinks/src/Bonmin/GamsBonmin.cpp
@@ -98,7 +98,7 @@ int main (int argc, char* argv[]) {
   smagReadModel (prob);
 
 #ifdef GAMS_BUILD
-	smagStdOutputPrint(prob, SMAG_ALLMASK, "\nGAMS/CoinBonmin MINLP Solver (Bonmin Library 0.99, using MUMPS Library 4.7.3)\nwritten by P. Bonami\n");
+	smagStdOutputPrint(prob, SMAG_ALLMASK, "\nGAMS/CoinBonmin MINLP Solver (Bonmin Library 0.99)\nwritten by P. Bonami\n");
 #else
 	smagStdOutputPrint(prob, SMAG_ALLMASK, "\nGAMS/Bonmin MINLP Solver (Bonmin Library 0.99)\nwritten by P. Bonami\n");
 #endif
