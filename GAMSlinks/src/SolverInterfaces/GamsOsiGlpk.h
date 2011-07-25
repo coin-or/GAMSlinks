a1cde7bb2558d915d1bbc674f2903b2ef39e4272
Stefan Vigerske <svigerske@gams.com>
2011-07-25 13:22:37 +0000

    CPL->EPL, no Id

--- GAMSlinks/src/SolverInterfaces/GamsOsiGlpk.h
+++ GAMSlinks/src/SolverInterfaces/GamsOsiGlpk.h
@@ -1,8 +1,6 @@
-// Copyright (C) GAMS Development and others 2009
+// Copyright (C) GAMS Development and others 2009-2011
 // All Rights Reserved.
-// This code is published under the Common Public License.
-//
-// $Id$
+// This code is published under the Eclipse Public License.
 //
 // Author: Stefan Vigerske
 
