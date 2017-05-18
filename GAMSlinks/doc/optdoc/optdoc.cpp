6ad345fac91769b50461cd0373b8f9c4bf7ba2c7
Stefan Vigerske <svigerske@gams.com>
2017-05-18 09:24:11 +0000

    escape backslash

--- GAMSlinks/doc/optdoc/optdoc.cpp
+++ GAMSlinks/doc/optdoc/optdoc.cpp
@@ -1785,7 +1785,7 @@ void printSCIPOptions()
          else if( strcmp(SCIPparamGetName(param), "display/width") == 0 )
             defaultdescr = "139 (80 for Windows without IDE)";
          else if( strcmp(SCIPparamGetName(param), "gams/mipstart") == 0 )
-            descr += ", see also section \ref SCIP_PARTIALSOL";
+            descr += ", see also section \\ref SCIP_PARTIALSOL";
 
          if( !hadadvanced && SCIPparamIsAdvanced(param) )
          {
