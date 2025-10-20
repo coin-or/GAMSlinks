d682003327854127ea5bb89327d168ac9b4b1d5d
Stefan Vigerske <svigerske@gams.com>
2025-10-20 15:18:03 +0200

    remove compatibility for old GAMS solverlinks API
    - all the C__ and D__ functions are no longer necessary

--- src/utils/GamsEntryPoints_tpl.c
+++ src/utils/GamsEntryPoints_tpl.c
@@ -24,14 +24,6 @@ typedef struct optRec* optHandle_t;
 #define GAMSSOLVER_CONCAT(a, b) GAMSSOLVER_CONCAT_(a, b)
 #endif
 
-#ifndef GAMSSOLVER_CONCAT3_
-#define GAMSSOLVER_CONCAT3_(a, b, c) a ## b ## c
-#endif
-
-#ifndef GAMSSOLVER_CONCAT3
-#define GAMSSOLVER_CONCAT3(a, b, c) GAMSSOLVER_CONCAT3_(a, b, c)
-#endif
-
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -62,12 +54,6 @@ DllExport int  STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,ModifyProblem)(void* Cptr
 /* Compatibility entry points so the solver link library can work with a GAMS version prior to GAMS 36 */
 
 /* Implemented below */
-DllExport void STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,Initialize)(void);
-
-/* Implemented below */
-DllExport void STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,Finalize)(void);
-
-/* Implemented below */
 DllExport void STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XCreate)(void** Cptr);
 
 /* Implemented below */
@@ -76,44 +62,10 @@ DllExport void STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XFree)(void** Cptr);
 /* Implemented below */
 DllExport int  STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg);
 
-/* Needs to be implemented by solver interface */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,ReadyAPI)(void* Cptr, struct gmoRec* Gptr, struct optRec* Optr);
-
-/* Needs to be implemented by solver interface */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,CallSolver)(void* Cptr);
-
 #ifdef GAMSSOLVER_HAVEMODIFYPROBLEM
-/* Needs to be implemented by solver interface if GAMSSOLVER_HAVEMODIFYPROBLEM is defined */
+/* Implemented below */
 DllExport int  STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,HaveModifyProblem)(void* Cptr);
-
-/* Needs to be implemented by solver interface if modify-problem is supported */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,HaveModifyProblem)(void* Cptr);
-
-/* Needs to be implemented by solver interface if modify-problem is supported */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,ModifyProblem)(void* Cptr);
 #endif
-
-/* Implemented below */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,XAPIVersion)(int api, char* Msg, int* comp);
-
-/* Implemented below */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(D__,GAMSSOLVER_ID,XAPIVersion)(int api, char* Msg, int* comp);
-
-/* Implemented below */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg);
-
-/* Implemented below */
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(D__,GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg);
-
-DllExport void STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,Initialize)(void)
-{
-   GAMSSOLVER_CONCAT(GAMSSOLVER_ID,Initialize)();
-}
-
-DllExport void STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,Finalize)(void)
-{
-   GAMSSOLVER_CONCAT(GAMSSOLVER_ID,Finalize)();
-}
 
 DllExport void STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XCreate)(void** Cptr)
 {
@@ -126,61 +78,17 @@ DllExport void STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XFree)(void** Cptr)
    GAMSSOLVER_CONCAT(GAMSSOLVER_ID,Free)(Cptr);
 }
 
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,ReadyAPI)(void* Cptr, struct gmoRec* Gptr, struct optRec* Optr)
+DllExport int  STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg)
 {
-   return GAMSSOLVER_CONCAT(GAMSSOLVER_ID,ReadyAPI)(Cptr, Gptr);
-}
-
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,CallSolver)(void* Cptr)
-{
-   return GAMSSOLVER_CONCAT(GAMSSOLVER_ID,CallSolver)(Cptr);
+   return 1;
 }
 
 #ifdef GAMSSOLVER_HAVEMODIFYPROBLEM
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,HaveModifyProblem)(void* Cptr)
-{
-   return GAMSSOLVER_CONCAT(GAMSSOLVER_ID,HaveModifyProblem)(Cptr);
-}
-
 DllExport int STDCALL GAMSSOLVER_CONCAT(GAMSSOLVER_ID,HaveModifyProblem)(void* Cptr)
 {
    return 0;
 }
-
-DllExport int  STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,ModifyProblem)(void* Cptr)
-{
-   return GAMSSOLVER_CONCAT(GAMSSOLVER_ID,ModifyProblem)(Cptr);
-}
 #endif
-
-/* comp returns the compatibility mode:
-   0: client is too old for the DLL, no compatibility
-   1: client version and DLL version are the same, full compatibility
-   2: client is older than DLL, but defined as compatible, backward compatibility
-   3: client is newer than DLL, forward compatibility
-   FIXME: for now, we just claim full compatibility
-*/
-DllExport int STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,XAPIVersion)(int api, char* Msg, int* comp)
-{
-  *comp = 1;
-  return 1;
-}
-
-DllExport int STDCALL GAMSSOLVER_CONCAT3(D__,GAMSSOLVER_ID,XAPIVersion)(int api, char* Msg, int* comp)
-{
-  *comp = 1;
-  return 1;
-}
-
-DllExport int STDCALL GAMSSOLVER_CONCAT3(C__,GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg)
-{
-   return 1;
-}
-
-DllExport int STDCALL GAMSSOLVER_CONCAT3(D__,GAMSSOLVER_ID,XCheck)(const char* funcn, int ClNrArg, int Clsign[], char* Msg)
-{
-   return 1;
-}
 
 #if defined(__cplusplus)
 }
